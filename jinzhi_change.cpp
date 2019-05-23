8823d8ecf31d9f986d9242097cb8f0965c7dfea8
DyingBunny <47047997+DyingBunny@users.noreply.github.com>
2019-05-23 16:43:24 +0800

    Add files via upload

---
+++ jinzhi_change.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+
+int main()
+{
+	std::string result = "";
+	std::string table = "0123456789ABCDEF";
+	int M=0, N=0;
+	std::cin >> M >> N;
+	while (M)
+	{
+		if (M < 0)
+		{
+			M = -M;
+			std::cout << "-";
+		}
+		result = table[M%N]+result;
+		M /= N;
+	}
+	std::cout << result << std::endl;
+	return 0;
+}
